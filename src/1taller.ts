f931dd9a0c0f9c70b1b8eae9a284eb4e53cc13d6
Byneros <ing.nelsonjulio@gmail.com>
2023-08-02 21:46:30 -0500

    commic

--- src/1taller.ts
+++ src/1taller.ts
@@ -22,33 +22,33 @@ document.addEventListener("DOMContentLoaded", () => {
   const input1 = document.getElementById("input1") as HTMLInputElement;
   const input2 = document.getElementById("input2") as HTMLInputElement;
   const resultado = document.getElementById("resultado") as HTMLParagraphElement;
-  const sumarBtn = document.getElementById("sumar") as HTMLButtonElement;
-  const restarBtn = document.getElementById("restar") as HTMLButtonElement;
-  const multiplicarBtn = document.getElementById("multiplicar") as HTMLButtonElement;
-  const dividirBtn = document.getElementById("dividir") as HTMLButtonElement;
+  const BotonSumar = document.getElementById("sumar") as HTMLButtonElement;
+  const BotonRestar = document.getElementById("restar") as HTMLButtonElement;
+  const BotonMultiplicar = document.getElementById("multiplicar") as HTMLButtonElement;
+  const BotonDividir = document.getElementById("dividir") as HTMLButtonElement;
 
-  sumarBtn.addEventListener("click", () => {
+  BotonSumar.addEventListener("click", () => {
       const num1 = parseFloat(input1.value);
       const num2 = parseFloat(input2.value);
       const result = sumar(num1, num2);
       resultado.textContent = `Resultado: ${result}`;
   });
 
-  restarBtn.addEventListener("click", () => {
+  BotonRestar.addEventListener("click", () => {
       const num1 = parseFloat(input1.value);
       const num2 = parseFloat(input2.value);
       const result = restar(num1, num2);
       resultado.textContent = `Resultado: ${result}`;
   });
 
-  multiplicarBtn.addEventListener("click", () => {
+  BotonMultiplicar.addEventListener("click", () => {
       const num1 = parseFloat(input1.value);
       const num2 = parseFloat(input2.value);
       const result = multiplicar(num1, num2);
       resultado.textContent = `Resultado: ${result}`;
   });
 
-  dividirBtn.addEventListener("click", () => {
+  BotonDividir.addEventListener("click", () => {
       const num1 = parseFloat(input1.value);
       const num2 = parseFloat(input2.value);
       try {
